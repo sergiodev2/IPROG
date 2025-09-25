f027eeea1364d84b26ee1ea25f63dd7939da88c4
sergiodev2 <sergiocastillaroncel@gmail.com>
2025-09-25 09:15:36 +0200

    Ejercicios php

---
+++ caracteresEspeciales.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>MI PRIMERA WEB EN PHP</h1>
+
+<?php
+echo "Este es un simbolo especial \$x";
+
+// Con esta barra "\" puedo poner caracteres especiales.
+?>
+
+</body>
+</html>
+
