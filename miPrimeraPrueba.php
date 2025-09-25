d2aca6cfe942aeda495720fc74bd60e78d291f11
sergiodev2 <sergiocastillaroncel@gmail.com>
2025-09-25 08:29:51 +0200

    IPROG

---
+++ miPrimeraPrueba.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>MI PRIMERA WEB EN PHP</h1>
+
+<?php
+echo "Hola IMWEB";
+// This is a single-line comment
+
+# This is also a single-line comment
+
+/* This is a
+multi-line comment */
+
+// Ejercicio3: Variables 
+$txt = "Hello world, hoy es 22 de septiembre !!";
+$x = 4 ;
+$y = 10.5;
+$edad = 19;
+$nombre = "Sergio Castilla";
+
+echo "<br>";
+echo "El valor de mi variable txt es: " . $txt ; //El punto (.) sirve para concatenar 
+echo "<br>";
+echo "<br>";
+echo "Mi edad es " . $edad . " y mi nombre es " . $nombre;
+?>
+
+</body>
+</html>
+
