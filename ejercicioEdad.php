8fac4bdb41b65e7415171bca3f9bb01239b51ab8
sergiodev2 <sergiocastillaroncel@gmail.com>
2025-10-03 10:04:41 +0200

    Ejercicios Condicionales

---
+++ ejercicioEdad.php
@@ -0,0 +1,19 @@
+<html>
+    <body>
+        <?php
+            $edadAlumno = 12;
+            $edadPadre = 36;
+
+            if ($edadAlumno >= 18) {
+                echo"Puede entrar al establecimiento";}
+            else {
+                echo"No puede entrar al establecimiento";
+            }
+            if ($edadPadre >= 30) {
+                echo"Ya tienes una edad para seguir comportandote como un niño";}
+            else {
+                echo"Eres joven todavía";
+            }
+        ?>
+    </body>
+</html>
---
+++ operadores.php
@@ -0,0 +1,11 @@
+<html>
+    <body>
+        <?php
+            # = asignación
+            # == Comparación del valor 
+            # === Comparación del valor y tipo 
+            # != son diferentes
+            # !== son diferentes en valor y tipo
+            ?>
+    </body>
+</html>
